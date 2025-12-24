0d2b05e65354b068a656cc48b53360bab17f8f8e
INGPaulG <168149100+INGPaulG@users.noreply.github.com>
2025-12-24 14:43:20 -0500

    Repo vacío

--- src/data/phrases.js
+++
@@ -1,9 +0,0 @@
-const phrases = [
-  "Eres mi universo favorito ✨",
-  "Cada estrella me recuerda a ti 🌙",
-  "Contigo, todo tiene sentido 💫",
-  "Eres la magia que no sabía que necesitaba 💖",
-  "Si me pierdo, que sea en tus ojos ⭐"
-];
-
-export default phrases;
--- src/pages/Message.jsx
+++
@@ -1,7 +0,0 @@
-export default function Message() {
-    return (
-        <div style={{ color: "white", padding: "40px" }}>
-            Mensaje 💌
-        </div>
-    );
- }
